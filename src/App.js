7218366f3b2434382e291af62abd080786cb02a4
jayjonas1996 <jayjonas1996@gmail.com>
2021-12-17 14:31:02 -0500

    Created ramp component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import RampComponent from './ramp.component';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <RampComponent input={undefined}></RampComponent>
+      <RampComponent input="Hello Ramp engineers :D"></RampComponent>
+      <RampComponent input={[1, 2, 3, 4]}></RampComponent>
     </div>
   );
 }
---
+++ src/ramp.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class RampComponent extends React.Component {
+  state = {
+    date: new Date(),
+  };
+
+  render() {
+    var dateFormatOption = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      minute: 'numeric',
+      hour: 'numeric',
+      second: 'numeric',
+    };
+
+    var inputValue = this.props.input;
+
+    if (!inputValue) {
+      window.setInterval(() => {
+        this.setState({
+          date: new Date(),
+        });
+      }, 500);
+      return (
+        <div>
+          {this.state.date.toLocaleDateString('en-US', dateFormatOption)}
+        </div>
+      );
+    } else if (inputValue instanceof Array) {
+      var elements = [];
+      inputValue.forEach((value) => {
+        elements.push(<div>{value}</div>);
+      });
+      return <div>{elements}</div>;
+    } else {
+      return <div>{inputValue.toString()}</div>;
+    }
+  }
+}
